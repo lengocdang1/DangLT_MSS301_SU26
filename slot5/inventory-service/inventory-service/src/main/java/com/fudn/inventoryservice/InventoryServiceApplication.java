6bacd69d69d90fe01ee38bf1e8b9b12a22f11455
lengocdang1 <lengocdang40@gmail.com>
2026-09-25 14:14:04 +0700

    TODO 5: feat: create Inventory model mapped to t_inventory table

--- slot5/inventory-service/inventory-service/src/main/java/com/fudn/inventoryservice/InventoryServiceApplication.java
+++ slot5/inventory-service/inventory-service/src/main/java/com/fudn/inventoryservice/InventoryServiceApplication.java
@@ -1,4 +1,4 @@
-package inventory_service;
+package com.fudn.inventoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
